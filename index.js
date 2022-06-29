2a171cc1cf545c38339bf1dd4a29a3c2b2d37be7
alexisar09 <78046280+alexisar09@users.noreply.github.com>
2022-06-29 13:43:18 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const practica = {
+Nombre : "Alexis",
+Edad : 19,
+Desarrollador : true,
+Nacimiento : new Date(2003,4,9),
+"Libro favorito" : {
+    Título: "22/11/63",
+    Autor: "Stephen King",
+    Fecha: new Date(2011, 10, 8),
+    Url: "https://www.google.com/search?client=opera&q=stephen+king+63&sourceid=opera&ie=UTF-8&oe=UTF-8"
+}
+}
+
+console.log(practica)
